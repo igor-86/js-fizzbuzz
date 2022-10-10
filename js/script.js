1e38866b5b6bf768fdf84f2f9c4f7f3398782643
igor-86 <i.campoli86@gmail.com>
2022-10-10 16:24:02 +0200

    Complete **Consegna**

--- js/script.js
+++ js/script.js
@@ -7,17 +7,23 @@ for (let n = 1; n <= 100; n++){
     let mulTre = "";
     let mulCinque = "";
     let mulTop = "";
-    if (n % 3 === 0){
-        mulTre = "Fizz";
+
+    if (n % 3 === 0 & n % 5 === 0){
+        mulTop = "FizBuzz";
+        console.log(mulTop);
+    } else if ( n % 3 === 0) {
+        mulTre = "Fizz"
+        console.log(mulTre);
     } else if ( n % 5 === 0) {
         mulCinque = "Buzz";
-    } else if (n % 3 === 0 & n % 5 === 0) {
-        mulTop = "Fizz Buzz";
+        console.log(mulCinque);
     } 
+
+    console.log(n);
 
   
 
-    console.log(n,mulTre,mulCinque,mulTop);
+    
    
     
 
